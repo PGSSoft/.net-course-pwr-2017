291397d162378b9ff29f580055e9e7b7ff998c23
BRodak <bartlomiej.rodak93a@gmail.com>
2017-03-17 08:22:41 +0100

    Spotkanie2 - Create MVC project

---
+++ PGSBoard/Controllers/BoardController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PGSBoard.Models;
+
+namespace PGSBoard.Controllers
+{
+    public class BoardController : Controller
+    {
+        public static GetBoardsViewModel GetBoardsViewModel { get; set; }
+        // GET: Board
+        static BoardController()
+        {
+            GetBoardsViewModel = 
+                new GetBoardsViewModel();
+        }
+
+        public ActionResult Index()
+        {
+            return View(GetBoardsViewModel);
+        }
+
+        public ActionResult CreateBoard()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateBoard(Board board)
+        {
+            GetBoardsViewModel.Boards.Add(board);
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class GetBoardsViewModel
+    {
+        public List<Board> Boards { get; set; }
+        public int SelectedBoardId { get; set; } = 3;
+        public IEnumerable<SelectListItem> BoardsItems 
+            => new SelectList(Boards, "Id", "Name");
+        public GetBoardsViewModel()
+        {
+            Boards = new List<Board>
+            {
+                new Board { Id = 1, Name = "First", Description = "xOXO"},
+                new Board { Id = 2, Name = "Second", Description = "xOXO2"},
+                new Board { Id = 3, Name = "Third", Description = "xOXO3"},
+                new Board { Id = 4, Name = "Fourth", Description = "xOXO4"}
+            };
+        }
+    }
+}
---
+++ PGSBoard/Models/Board.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PGSBoard.Models
+{
+    public class Board
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+    }
+}
