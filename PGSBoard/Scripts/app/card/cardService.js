1302d2b8b1d350d18a7c9884329b18ac69b8e90f
szymczakk <rakaz666@gmail.com>
2017-03-30 20:06:03 +0200

    final, working version

--- PGSBoard/Scripts/app/card/cardService.js
+++ PGSBoard/Scripts/app/card/cardService.js
@@ -1,3 +1,28 @@
-﻿(function (undefined) {
-    alert("it works!");
-})();
+﻿$(document).ready(function () {
+    $('.card').on('click', '.card__remove-card-btn', deleteCard);
+
+    function deleteCard(event) {
+        var cardId = +$(event.target).data('cardId');
+
+        $.ajax({
+            method: "DELETE",
+            url: "/Board/DeleteCard",
+            success: onSuccess,
+            error: onError,
+            data: { cardId: cardId }
+            
+        });
+
+        function onSuccess(data) {
+            if (isNaN(data) || data === 0) {
+                return;
+            }
+
+            $(event.target).closest(".card").remove();
+        }
+
+        function onError(data) {
+
+        }
+    }
+});
